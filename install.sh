63d6d8d416e3fbcca0f3bdba9fd85d2329c16420
filusch <filusch@cirr-it.de>
2023-05-26 09:42:56 +0200

    fix readme

--- install.sh
+++ install.sh
@@ -4,24 +4,21 @@ if [ "$EUID" -eq 0 ]
 fi
 
 EXTENSION_TARGET="${HOME}/klipper/klippy/extras"
-MAIN_SCRIPT_NAME="linear_movement_vibrations.py"
-EXTRAS_SCRIPT_NAME="linear_movement_plot_lib_stat.py"
+declare -a SCRIPTS=("linear_movement_vibrations.py" "linear_movement_plot_lib_stat.py")
 GIT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-function link_extension_file (){
+echo -e "Installation Script for Klipper Linear Movement Analysis by MarschallMarc#6420"
+echo -e "Linking extension file"
+
+for i in "${SCRIPTS[@]}"
+do
     if [ -d "${EXTENSION_TARGET}" ]; then
-        rm -f "${EXTENSION_TARGET}/${$1}"
-        ln -sf "${GIT_DIR}/${SCRIPT_NAME}" "${EXTENSION_TARGET}/${$1}"
+        rm -f "${EXTENSION_TARGET}/$i"
+        ln -sf "${GIT_DIR}/$i" "${EXTENSION_TARGET}/$i"
     else
         echo -e "${EXTENSION_TARGET} not found, exiting installation."
         exit 1
     fi
-}
+done
 
-
-echo -e "Installation Script for Klipper Linear Movement Analysis by MarschallMarc#6420"
-echo -e "Linking extension file"
-link_extension_file MAIN_SCRIPT_NAME
-link_extension_file EXTRAS_SCRIPT_NAME
-~/klippy-env/bin/pip install -v matplotlib
-~/klippy-env/bin/pip install -v scipy
+~/klippy-env/bin/pip install -v matplotlib scipy
